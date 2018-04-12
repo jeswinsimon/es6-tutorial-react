0a30e8c364561a7c8b5466941ce0894851175fa9
jeswinsimon <jeswinsimon@gmail.com>
2018-04-12 08:10:23 +0530

    Creating Modules

--- js/mortgageCalculator.js
+++ js/mortgageCalculator.js
@@ -1,25 +1,6 @@
 import React from 'react'
 import AmortizationChart from './AmortizationChart'
-
-let calculatePayment = (principal, years, rate) => {
-    let monthlyRate = rate / 100 / 12;
-    let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1/(1 + monthlyRate), years * 12)));
-    let balance = principal;
-    let amortization = [];
-    for (let y=0; y<years; y++) {
-        let interestY = 0;  //Interest payment for year y
-        let principalY = 0; //Principal payment for year y
-        for (let m=0; m<12; m++) {
-            let interestM = balance * monthlyRate;       //Interest payment for month m
-            let principalM = monthlyPayment - interestM; //Principal payment for month m
-            interestY = interestY + interestM;
-            principalY = principalY + principalM;
-            balance = balance - principalM;
-        }
-        amortization.push({principalY, interestY, balance});
-    }
-    return {monthlyPayment: monthlyPayment, amortization:amortization};
-};
+import * as mortgage from './mortgage'
 
 class MortgageCalculator extends React.Component{
     constructor(props) {
@@ -40,7 +21,7 @@ class MortgageCalculator extends React.Component{
         this.setState({rate: event.target.value});
     }
     render () {
-        let {monthlyPayment, amortization} = calculatePayment(this.state.principal, this.state.years, this.state.rate);
+        let {monthlyPayment, amortization} = mortgage.calculatePayment(this.state.principal, this.state.years, this.state.rate);
         return (
             <div className="content">
                 <div className="form">
